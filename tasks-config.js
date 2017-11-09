f87c17b59bda11d3d682293b8d19187855fe6b78
syuji-higa <higa@trymore-inc.jp>
2017-11-09 14:12:14 +0900

    スペル修正

--- tasks-config.js
+++ tasks-config.js
@@ -36,7 +36,7 @@ export default {
     imports       : 'webpack/imports',
     dest          : 'htdocs',
     minify        : true,
-    notMinifyFiles: ['webpack/**/vender.js'],
+    notMinifyFiles: ['webpack/**/vendor.js'],
   },
   images: {
     root      : 'images',
