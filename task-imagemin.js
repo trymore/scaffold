f6134b765d4898ed4d0ab7301da88b32951a760e
takashi-higa <takashi-higa@trymore-inc.jp>
2018-08-02 14:11:07 +0900

    npm run imageminコマンドが動くように修正

--- task-imagemin.js
+++ task-imagemin.js
@@ -1,5 +1,13 @@
 import Imagemin from './tasks/imagemin';
+import minimist from 'minimist';
 
-NS.argv = minimist(process.argv.slice(2));
+if(!global.NS) {
+  global.NS = {};
+  NS.argv = minimist(process.argv.slice(2));
+  NS.isFirstBuild = true;
+  NS.curtFiles    = {
+    imageminSet: new Set()
+  };
+}
 
 new Imagemin().start();
